c6d8563a6218ada9367f4f183629762cc51c9ca7
Mayank <nathanimayank31@gmail.com>
2026-03-25 16:08:09 +0530

    Added grading logic program in Python

---
+++ hello world.py
@@ -0,0 +1,3 @@
+print(1+2)
+a= [1,2,3,True,2.33]
+print(a)
---
+++ tudedudetask.py
@@ -0,0 +1,17 @@
+score=int(input())
+if score>90 :
+    print("Grade=A")
+
+elif score>79 and score<90 :
+    print("Grade=B")
+
+elif score>69 and score<80:
+    print("Grade=C")
+
+elif score>59 and score<70:
+    print("Grade=D")
+
+else:
+    print("Grade=F")
+ 
+
