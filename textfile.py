00d1e68bf320e699b87dcb697a504bb60a8d281b
Mayank <nathanimayank31@gmail.com>
2026-03-27 10:28:02 +0530

    Adding text file code

---
+++ textfile.py
@@ -0,0 +1,7 @@
+file = open("sample.txt", "w")   
+
+file.write("Hello, this is my first file.\n")
+file.write("Learning Python file handling.\n")
+
+file.close() 
+print("File created and data written successfully.")
