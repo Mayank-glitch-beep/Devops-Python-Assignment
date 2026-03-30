40967650e924cc144b692344b7eb29d54ff23486
Mayank <nathanimayank31@gmail.com>
2026-03-30 11:34:03 +0530

    Adding output files

--- tudedudetask.py
+++ tudedudetask.py
@@ -1,17 +1,19 @@
-score=int(input())
-if score>90 :
-    print("Grade=A")
+while(True):
+    score=int(input())
+    if score>90 :
+        print("Grade=A")
+    elif score>79 and score<90 :
+        print("Grade=B")
+    elif score>69 and score<80:
+        print("Grade=C")
+    elif score>59 and score<70:
+        print("Grade=D")
+    else:
+        print("Grade=F")
+    print("If you want to exit press 0")
+    if score==0:
+        break
 
-elif score>79 and score<90 :
-    print("Grade=B")
 
-elif score>69 and score<80:
-    print("Grade=C")
-
-elif score>59 and score<70:
-    print("Grade=D")
-
-else:
-    print("Grade=F")
  
 
